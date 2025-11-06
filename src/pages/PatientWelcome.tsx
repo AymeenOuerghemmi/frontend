f589b1f7794004e18d395783288f7979e8d3e68c
Aymen Ouerghemmi <159343354+AymeenOuerghemmi@users.noreply.github.com>
2025-11-06 12:18:39 +0100

    update

--- src/pages/PatientWelcome.tsx
+++ src/pages/PatientWelcome.tsx
@@ -244,7 +244,7 @@ export default function PatientWelcome() {
           className="rounded-2xl shadow-lg object-cover transition-transform hover:scale-[1.06]"
           style={{
             width: "200px",   // largeur augmentée
-            height: "120px",  // ✅ hauteur augmentée ici
+            height: "180px",  // ✅ hauteur augmentée ici
             borderRadius: "18px",
           }}
         />
